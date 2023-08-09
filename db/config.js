4bc93ec0f74f90aa6ad80fc29eda15c147014528
PabloOro27 <pabloorozco2712@gmail.com>
2023-08-09 00:36:01 -0600

    configuracion

--- db/config.js
+++ db/config.js
@@ -1,25 +1,40 @@
-// configuracion de la base de datos para migracion
-// traemos las configuraciones de la base de datos
+// Traemos las configuraciones de la base de datos
 const { config } = require('../config/config.js');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mssql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
     url: URI,
-    dialect: 'mysql',
+    dialect: 'mssql', // Cambiamos 'mysql' a 'mssql'
+    dialectOptions: {
+      options: {
+        encrypt: true, // Si estás utilizando una conexión segura, activa el cifrado
+      },
+    },
     // logging: false,
   },
   production: {
     url: URI,
-    dialect: 'mysql',
+    dialect: 'mssql', // Cambiamos 'mysql' a 'mssql'
+    dialectOptions: {
+      options: {
+        encrypt: true, // Si estás utilizando una conexión segura, activa el cifrado
+      },
+    },
     // logging: false,
   },
   test: {
     url: URI,
-    dialect: 'mysql',
+    dialect: 'mssql', // Cambiamos 'mysql' a 'mssql'
+    dialectOptions: {
+      options: {
+        encrypt: true, // Si estás utilizando una conexión segura, activa el cifrado
+      },
+    },
     // logging: false,
   },
+  loggin: console.log,
 };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,22 +1,27 @@
 const { Sequelize } = require('sequelize');
 
-// configuracion de la base de datos
+// Configuración de la base de datos
 const { config } = require('../config/config.js');
-// configuracion de los modelos de la base de datos
+// Configuración de los modelos de la base de datos
 const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mssql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-// instanciamos el constructor de sequelize
-const sequelize = new Sequelize(URI,{
-  dialect: 'mysql',
+// Instanciamos el constructor de Sequelize
+const sequelize = new Sequelize(URI, {
+  dialect: 'mssql', // Cambiamos 'mysql' a 'mssql'
+  dialectOptions: {
+    options: {
+      encrypt: true, // Si estás utilizando una conexión segura, activa el cifrado
+    },
+  },
   logging: true,
 });
 
 setupModels(sequelize);
-// sincronizamos la base de datos
+// Sincronizamos la base de datos
 sequelize.sync();
 
 module.exports = sequelize;
